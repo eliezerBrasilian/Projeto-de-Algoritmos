0566d5cac2114aec1bc1a7b733288ffd34d445e7
Eliezer Assuncao <eliezerassuncaocustodio@gmail.com>
2023-11-13 20:15:51 -0300

    fixed wrong sort on Shell Sort (ordenação no arquivo de saída deveria estar decrescente e estava sendo crescente)

--- classes/ShellSort.cpp
+++ classes/ShellSort.cpp
@@ -53,7 +53,7 @@ void ShellSort::decrescente(int n,int *arr){
     for (intervalo = n / 2; intervalo > 0; intervalo /= 2) {
         for (i = intervalo; i < n; i++) {
             temp = arr[i];
-            for (j = i; j >= intervalo && arr[j - intervalo] > temp; j -= intervalo) {
+            for (j = i; j >= intervalo && arr[j - intervalo] < temp; j -= intervalo) {
                 arr[j] = arr[j - intervalo];
             }
             arr[j] = temp;
